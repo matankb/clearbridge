6cbfc1fb48742817f2a4752f2203fd7a89e105d5
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-04-03 19:27:16 -0400

    Fix callback URL

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
   google: {
     clientID: '912811149086-rhb4j88j3p0ns5tp4ijs2g2kql81cfu4.apps.googleusercontent.com',
     clientSecret: 'EiYNNkSRbpcne6EImk3AgD27',
-    callbackURL: production ? 'https://bridge-jcdsboston.rhcloud.com/callback' : 'http://127.0.0.1/auth/callback',
+    callbackURL: production ? 'https://bridge-jcdsboston.rhcloud.com/callback' : 'http://127.0.0.1:8080/auth/callback',
   },
   db: {
     url: production ? process.env.OPENSHIFT_MONGODB_DB_URL : 'mongodb://127.0.0.1/bridge',
