7bf4995bdf6098381b80089c4a7ccd737e1659ca
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-04-05 15:28:50 -0400

    Create login-protected middleware

--- server/middleware/login-protected.js
+++ server/middleware/login-protected.js
@@ -1,6 +1,6 @@
 /* NOTE: login-protected middleware redirects to login if not authenticated
  * whearas ensure-authenticated ends request if not authenticated
- * This module to be used in protected routes that will be NOT accessed by user, such as api
+ * This module to be used in protected routes that will be accessed by user, such as dashboard
 */
 
 // TODO: find better name for module
@@ -9,6 +9,6 @@ module.exports = function(req, res, next) {
   if (req.user) {
     next();
   } else {
-    res.end();
+    res.redirect('/');
   }
 };
