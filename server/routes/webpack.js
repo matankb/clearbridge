6e7ec7f9da864f19303d648613524857d6ece46d
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-04-27 09:40:19 -0400

    HOTFIX

--- server/routes/webpack.js
+++ server/routes/webpack.js
@@ -1,10 +1,9 @@
-const webpack = require('webpack');
-const webpackConfig = require('../../webpack.config');
-const compiler = webpack(webpackConfig);
-
 module.exports = function(app) {
 
   if (process.env.NODE_ENV !== 'production') {
+    const webpack = require('webpack');
+    const webpackConfig = require('../../webpack.config');
+    const compiler = webpack(webpackConfig);
     app.use(require('webpack-dev-middleware')(compiler, {
       noInfo: true,
       publicPath: webpackConfig.output.publicPath,
