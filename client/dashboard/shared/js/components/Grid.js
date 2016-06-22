cbcfa41d790b618d791e28529eacf5bdbcdbb090
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-06-22 09:51:51 -0400

    Implement shared grid system

---
+++ client/dashboard/shared/js/components/Grid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../../css/grid.css';
+
+const Grid = ({ children }) => <div className="grid">{ children }</div>;
+const GridTileSmall = ({ children }) => <div className="grid-tile-small">{ children }</div>;
+const GridTileWide = ({ children }) => <div className="grid-tile-wide">{ children }</div>;
+const GridTileLarge = ({ children }) => <div className="grid-tile-large">{ children }</div>;
+
+export default Grid;
+export { GridTileSmall, GridTileWide, GridTileLarge };
