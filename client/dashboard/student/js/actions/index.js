6d1daa94a148fca65c7c87a1b810424bcbc14592
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-06-22 09:47:47 -0400

    Move action constants to top of file

--- client/dashboard/student/js/actions/index.js
+++ client/dashboard/student/js/actions/index.js
@@ -1,29 +1,34 @@
 export const REQUEST_TOPIC = 'REQUEST_TOPIC';
+export const RECEIVE_TOPIC = 'RECEIVE_TOPIC';
+export const REQUEST_TOPIC_LIST = 'REQUEST_TOPIC_LIST';
+export const RECEIVE_TOPIC_LIST = 'RECEIVE_TOPIC_LIST';
+export const FETCH_TOPIC_LIST = 'FETCH_TOPIC_LIST';
+export const FETCH_TOPIC = 'FETCH_TOPIC';
+export const SELECT_TOPIC = 'SELECT_TOPIC';
+export const TOGGLE_TOPIC_PAGE = 'TOGGLE_TOPIC_PAGE';
+
 export function requestTopic(topic) {
   return {
     type: REQUEST_TOPIC,
-    name: topic.name,
     id: topic.id,
   };
 }
 
-export const RECEIVE_TOPIC = 'RECEIVE_TOPIC';
 export function receiveTopic(topic) {
   return {
     type: RECEIVE_TOPIC,
-    name: topic.name,
     id: topic.id,
+    blurb: topic.blurb,
+    sections: topic.sections,
   };
 }
 
-export const REQUEST_TOPIC_LIST = 'REQUEST_TOPIC_LIST';
 export function requestTopicList() {
   return {
     type: REQUEST_TOPIC_LIST,
   };
 }
 
-export const RECEIVE_TOPIC_LIST = 'RECEIVE_TOPIC_LIST';
 export function receiveTopicList(topics) {
   return {
     type: RECEIVE_TOPIC_LIST,
@@ -31,7 +36,6 @@ export function receiveTopicList(topics) {
   };
 }
 
-export const FETCH_TOPIC_LIST = 'FETCH_TOPIC_LIST';
 export function fetchTopicList(user) {
 
   return function(dispatch) {
