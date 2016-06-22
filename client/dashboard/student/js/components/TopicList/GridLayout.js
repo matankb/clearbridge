a58377e89e25577dc7bb5f3af9a0d36fff183a50
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-06-22 09:52:30 -0400

    Create TopicList component

---
+++ client/dashboard/student/js/components/TopicList/GridLayout.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+import Grid, {
+  GridTileSmall as SmallTile,
+  GridTileWide as WideTile,
+  GridTileLarge as LargeTile,
+} from '../../../../shared/js/components/Grid';
+
+import '../../../../shared/css/grid.css';
+
+const GridLayout = ({ items }) => {
+  return (
+    <Grid>
+      {
+        items[0] ?
+        <LargeTile>
+          { items[0] }
+        </LargeTile> :
+        null
+      }
+      {
+        items[1] ?
+        <LargeTile>
+          <WideTile>
+            <SmallTile>{ items[1] }</SmallTile>
+            <SmallTile>{ items[2] }</SmallTile>
+          </WideTile>
+          <WideTile>{ items[3] }</WideTile>
+        </LargeTile> :
+        null
+      }
+      {
+        items[4] ?
+        <LargeTile>
+          <WideTile>
+            { items[4] }
+          </WideTile>
+          <WideTile>
+            <SmallTile>{ items[5] }</SmallTile>
+            <SmallTile>{ items[6] }</SmallTile>
+          </WideTile>
+        </LargeTile> :
+        null
+      }
+      {
+        items[7] ?
+        <LargeTile>
+          <WideTile>
+            <SmallTile>{ items[7] }</SmallTile>
+            <SmallTile>{ items[8] }</SmallTile>
+          </WideTile>
+          <WideTile>
+            <SmallTile>{ items[9] }</SmallTile>
+            <SmallTile>{ items[10] }</SmallTile>
+          </WideTile>
+        </LargeTile> :
+        null
+      }
+      {
+        items[11] ?
+        <LargeTile>
+          <WideTile>
+            <SmallTile>{ items[11] }</SmallTile>
+            <SmallTile>{ items[12]}</SmallTile>
+          </WideTile>
+          <WideTile>
+            { items[13] }
+          </WideTile>
+        </LargeTile> :
+        null
+      }
+      {
+        items[14] ?
+        <LargeTile>
+          { items[14] }
+        </LargeTile> :
+        null
+      }
+      {
+        items[15] ?
+        <LargeTile>
+          <WideTile>{ items[15] }</WideTile>
+          <WideTile>{ items[16] }</WideTile>
+        </LargeTile> :
+        null
+      }
+      {
+        items[16] ?
+        <LargeTile>
+          { items[16] }
+        </LargeTile> :
+        null
+      }
+    </Grid>
+  )
+}
+
+export default GridLayout
---
+++ client/dashboard/student/js/components/TopicList/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchTopicList } from '../../actions';
+
+import GridLayout from './GridLayout';
+import TopicCard from '../TopicCard';
+
+class TopicList extends React.Component {
+
+  componentWillMount() {
+    this.props.fetchTopics(); // load topic list into redux store
+  }
+
+  render() {
+
+    const topicCards = this.props.topics.map(topic => {
+      return (
+        <TopicCard
+          name={ topic.name }
+          image={ topic.image }
+          color={ topic.color }
+          id={ topic.id }
+        />
+      );
+    });
+    return (
+      <div className="topic-list">
+        <GridLayout items={ topicCards } />
+      </div>
+    );
+
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isFetching: state.isFetchingTopicList,
+    topics: state.topics,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchTopics: () => {
+      dispatch(fetchTopicList());
+    },
+  };
+}
+
+TopicList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TopicList);
+
+export default TopicList;
