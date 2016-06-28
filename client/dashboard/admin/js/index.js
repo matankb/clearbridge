abc3c05823e538dfd8e7ee5e000a5353e8921b2a
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-06-28 10:47:22 -0400

    Initialize admin dashboard

---
+++ client/dashboard/admin/js/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import App from './App';
+import rootReducer from './reducers';
+
+injectTapEventPlugin();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware)
+);
+
+ReactDOM.render(
+  <AppContainer><App store={ store } /></AppContainer>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    let NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer><NextApp store={ store } /></AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
