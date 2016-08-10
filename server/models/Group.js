2142f578e9a7e4b258ead51e5acb79110631e404
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-10 12:54:14 -0400

    Implement Group schema

---
+++ server/models/Group.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema({
+  name: String,
+  students: [],
+  /* filter is fn that is run with new student array filter method.
+   * should take student obj. and return true if applies is applied to this group.
+  */
+  filter: Schema.Types.Mixed,
+});
+
+groupSchema.methods.addNewUsers = function(users) {
+  this.students.push(
+    ...users.filter(this.filter)
+  );
+  // TODO: setup error handling
+  this.save();
+};
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
