6dd2f3966cf1e043270cdb85fa7f7307d84329dc
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-10 13:11:37 -0400

    Move SERVER constants into config.js

--- server/config.js
+++ server/config.js
@@ -2,9 +2,14 @@ module.exports = {
   google: {
     clientID: '912811149086-rhb4j88j3p0ns5tp4ijs2g2kql81cfu4.apps.googleusercontent.com',
     clientSecret: 'EiYNNkSRbpcne6EImk3AgD27',
-    callbackURL: process.env.NODE_ENV ? 'https://bridge-jcdsboston.rhcloud.com/callback' : 'http://127.0.0.1:8080/auth/callback',
+    callbackURL: process.env.NODE_ENV ?
+                'https://bridge-jcdsboston.rhcloud.com/callback' :
+                'http://127.0.0.1:8080/auth/callback',
   },
+  // allow for both local and openshift envs
   db: {
-    url: process.env.NODE_ENV === 'production' ? process.env.OPENSHIFT_MONGODB_DB_URL : 'mongodb://127.0.0.1/bridge',
+    url: process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1/bridge',
   },
+  SERVER_PORT: process.env.NODE_PORT || 8080,
+  SERVER_IP: process.env.NODE_IP || '127.0.0.1',
 };
--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const config = require('./config');
 const routes = require('./routes');
 const auth = require('./auth');
 
-// allow for both local and openshift environments
-const SERVER_PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const SERVER_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
 mongoose.Promise = global.Promise; // use es6 promises in mongoose queries
 mongoose.connect(config.db.url);
 
