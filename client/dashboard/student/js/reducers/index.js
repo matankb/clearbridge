9a1f414de6ca7289342e16265e994347cd062855
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-10 12:59:58 -0400

    Convert to object rest spread in student reducers

--- client/dashboard/student/js/reducers/index.js
+++ client/dashboard/student/js/reducers/index.js
@@ -14,52 +14,59 @@ function topics(state = defaultState, action) {
       });
 
     case 'RECEIVE_TOPIC_LIST':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetchingTopicList: false,
         topics: action.topics.map(topic => {
-          return Object.assign({}, topic, {
+          return {
+            ...topic,
             isFetching: false,
             hasFull: false,
-          });
+          };
         }),
-      });
+      };
 
     case 'REQUEST_TOPIC':
-      return Object.assign({}, state, {
-        topics: state.topics.map(topic => {
-          if (topics.id === action.id) {
-            return Object.assign({}, topic, { isFetching: true });
-          } else {
-            return topics;
-          }
-        }),
-      });
-
-    case 'RECEIVE_TOPIC':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         topics: state.topics.map(topic => {
           if (topic.id === action.id) {
-            return Object.assign({}, topic, {
-              isFetching: false,
-              hasFull: true,
-              blurb: action.blurb,
-              sections: action.sections,
-            });
+            return { ...topic, isFetching: true };
           } else {
             return topic;
           }
         }),
-      });
+      };
+
+    case 'RECEIVE_TOPIC':
+      return {
+        ...state,
+        topics: state.topics.map(topic => {
+          if (topic.id === action.id) {
+            return {
+              ...topic,
+              isFetching: false,
+              hasFull: true,
+              blurb: action.blurb,
+              sections: action.sections,
+            };
+          } else {
+            return topic;
+          }
+        }),
+      };
 
     case 'SELECT_TOPIC':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         selectedTopic: action.id,
-      });
+      };
 
     case 'TOGGLE_TOPIC_PAGE':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         topicPageOpen: !state.topicPageOpen,
-      });
+      };
 
     default:
       return state;
