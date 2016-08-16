a19f0a367d223e279f098ffba943f104ef175979
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-16 19:01:40 -0400

    Add four more color constants

--- client/dashboard/shared/js/constants/colors.js
+++ client/dashboard/shared/js/constants/colors.js
@@ -4,6 +4,10 @@ const colors = {
     dark: '#00796B',
   },
   accent: '#00BCD4',
+  highlight: '#EDF4FD',
+  gray: 'rgba(0, 0, 0, 0.58)',
+  lightGray: 'rgba(0, 0, 0, 0.1)',
+  background: '#f4f4f4',
 };
 
 export default colors;
