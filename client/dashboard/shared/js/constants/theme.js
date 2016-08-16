c808cd3a50674031602f47a09a7d940144201ef2
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-16 19:01:21 -0400

    Set global material-ui theme

---
+++ client/dashboard/shared/js/constants/theme.js
@@ -0,0 +1,15 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import {
+  teal500,
+  teal700,
+  cyan500,
+} from 'material-ui/styles/colors';
+
+export default getMuiTheme({
+  palette: {
+    primary1Color: teal500,
+    primary2Color: teal700,
+    accent1Color: cyan500,
+  },
+});
