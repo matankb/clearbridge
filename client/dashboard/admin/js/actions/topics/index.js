a6b851008aa842303f0aee3fe99c89f7ad312315
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-17 11:43:44 -0400

    Make topic creation reducers

--- client/dashboard/admin/js/actions/topics/index.js
+++ client/dashboard/admin/js/actions/topics/index.js
@@ -1,6 +1,44 @@
+export const SELECT_CREATION_STAGE = 'TOPICS/SELECT_CREATION_STAGE';
+export const NEXT_CREATION_STAGE = 'TOPICS/NEXT_CREATION_STAGE';
+export const SET_CREATION_STATUS = 'TOPICS/SET_CREATION_STATUS';
+export const SET_CREATION_DATA = 'TOPICS/SET_CREATION_DATA';
+export const RESET_CREATION_DATA = 'TOPICS/RESET_CREATION_DATA';
 export const REQUEST_TOPICS = 'TOPICS/REQUEST_TOPICS';
 export const RECEIVE_TOPICS = 'TOPICS/RECEIVE_TOPICS';
 export const SELECT_TOPIC = 'TOPICS/SELECT_TOPIC';
+
+export function selectCreationStage(stage) {
+  return {
+    type: SELECT_CREATION_STAGE,
+    stage,
+  };
+}
+
+export function nextCreationStage() {
+  return {
+    type: NEXT_CREATION_STAGE,
+  };
+}
+
+export function setCreationStatus(isCreating) {
+  return {
+    type: SET_CREATION_STATUS,
+    isCreating,
+  };
+}
+
+export function setCreationData(data) {
+  return {
+    type: SET_CREATION_DATA,
+    data,
+  };
+}
+
+export function resetCreationData() {
+  return {
+    type: RESET_CREATION_DATA,
+  };
+}
 
 export function requestTopics() {
   return {
