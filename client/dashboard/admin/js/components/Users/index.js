c5565845bdc792989776c16cc8fa4156517f4100
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-16 20:00:01 -0400

    Show create user dialog on user page

--- client/dashboard/admin/js/components/Users/index.js
+++ client/dashboard/admin/js/components/Users/index.js
@@ -1,3 +1,4 @@
+import CreateUser from './CreateUser';
 class Users extends React.Component {
   componentWillMount() {
     this.props.fetchUsers();
@@ -5,6 +6,13 @@ class Users extends React.Component {
   render() {
     return (
       <div className="users">
+        <FloatingActionButton
+          style={ style.fab }
+          backgroundColor={ colors.accent }
+          onClick={ this.props.handleFABClick }
+        >
+          <AddIcon />
+        <CreateUser open={ this.props.isCreating } />
       </div>
     );
   }
@@ -12,16 +20,20 @@ class Users extends React.Component {
 
 function mapStateToProps(state) {
   return {
+    isCreating: state.users.create.isCreating,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     fetchUsers: () => dispatch(fetchUsers()),
+    handleFABClick: () => dispatch(setCreationStatus(true)),
   };
 }
+
 Users = connect(
   mapStateToProps,
   mapDispatchToProps
 )(Users);
+
 export default Users;
