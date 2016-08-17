3e7a6cb18686e8a830e3a03024e4dbaf3db8144e
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-16 20:32:48 -0400

    Render topic card based on api-returned topics array

---
+++ client/dashboard/admin/js/components/Topics/TopicList/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TopicCard from './TopicCard/';
+
+const TopicList = props => {
+  return (
+    <div className="topic-list">
+      {
+        props.topics.map(topic => {
+          return (
+            <TopicCard
+              id={ topic._id }
+              key={ topic._id }
+              name={ topic.name }
+              blurb={ topic.blurb }
+              image={ topic.image }
+              color={ topic.color }
+              handleTopicClick= {
+                () => {
+                  props.setSelectedTopic(topic._id)();
+                  props.toggleTopicPage();
+                }
+              }
+            />
+          );
+        })
+      }
+    </div>
+  );
+};
+
+export default TopicList;
