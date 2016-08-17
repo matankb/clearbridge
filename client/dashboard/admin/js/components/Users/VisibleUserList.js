b2e9e7a09c27e0c2c15444f07a6a71487baf4950
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-16 20:04:21 -0400

    Filter users in user list based on visibility filter

---
+++ client/dashboard/admin/js/components/Users/VisibleUserList.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+
+import UserList from './UserList';
+import { setVisibilityFilter } from '../../actions/users';
+
+function getVisibleUserList(users, filter) {
+  switch (filter) {
+    case 'ALL':
+      return users;
+    case 'STUDENTS':
+      return users.filter(user => user.type === 0);
+    case 'TEACHERS':
+      return users.filter(user => user.type === 1);
+    default:
+      return users;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    users: getVisibleUserList(state.users.userList.users, state.users.userList.visibilityFilter),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setVisibilityFilter: dispatch(setVisibilityFilter()),
+  };
+}
+
+const VisibileUserList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserList);
+
+export default VisibileUserList;
