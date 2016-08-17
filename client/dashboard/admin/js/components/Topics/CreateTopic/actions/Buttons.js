e00f88a5b7bd009a20f7f76e51feacbf7e94f522
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-08-17 11:12:33 -0400

    Create buttons to appear on every topic creation stage

---
+++ client/dashboard/admin/js/components/Topics/CreateTopic/actions/Buttons.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  buttonWrap: {
+    textAlign: 'right',
+  },
+  button: {
+    marginRight: 10,
+  },
+};
+
+const Actions = props => {
+  return (
+    <div style={ style.buttonWrap }>
+      <FlatButton
+        label="Cancel"
+        style={ style.button }
+        onTouchTap={ props.handleCancel }
+      />
+    { !props.final &&
+        <RaisedButton
+          label="Next"
+          primary
+          style={ style.button }
+          onTouchTap={ props.handleNextStage }
+        />
+      }
+      { props.final &&
+        <RaisedButton
+          label="Create"
+          primary
+          style={ style.button }
+          onTouchTap={ props.handleCreate }
+        />
+      }
+    </div>
+  );
+};
+
+export default Actions;
---
+++ client/dashboard/admin/js/components/Topics/CreateTopic/actions/index.js
@@ -0,0 +1,40 @@
+import { connect } from 'react-redux';
+
+import Buttons from './Buttons';
+import {
+  nextCreationStage,
+  setCreationStatus,
+  resetCreationData,
+  selectCreationStage,
+  postTopic,
+} from '../../../../actions/topics';
+
+function mapStateToProps(state, ownProps) {
+  return {
+    final: state.topics.create.stage === ownProps.stages - 1,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    handleNextStage: () => dispatch(nextCreationStage()),
+    handleCancel: () => {
+      dispatch(setCreationStatus(false));
+      dispatch(resetCreationData());
+      dispatch(selectCreationStage(0));
+    },
+    handleCreate: () => {
+      dispatch(postTopic());
+      dispatch(setCreationStatus(false));
+      dispatch(resetCreationData());
+      dispatch(selectCreationStage(0));
+    },
+  };
+}
+
+const actions = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Buttons);
+
+export default actions;
