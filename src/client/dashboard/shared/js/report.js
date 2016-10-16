0c02c8580d8c9fd547384aef4e87ab2279a70eef
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-10-16 16:05:01 -0400

    Implement genereric client-side report routine

---
+++ src/client/dashboard/shared/js/report.js
@@ -0,0 +1,32 @@
+import fetch from 'fetch-reject';
+
+// report types
+export const SUGGESTION = 0;
+export const PROBLEM = 1;
+export const ERROR = 2;
+
+// returns serializable navigator object
+function getNavigator() {
+  let _navigator = {};
+  for (let i in navigator) { // eslint-disable-line guard-for-in
+    _navigator[i] = navigator[i];
+  }
+  return _navigator;
+}
+
+// generic report routine. Passes any data for server to handle
+export default function sendReport(type, data) {
+  return fetch('/api/report/', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      type,
+      data,
+      navigator: getNavigator(),
+    }),
+  });
+}
