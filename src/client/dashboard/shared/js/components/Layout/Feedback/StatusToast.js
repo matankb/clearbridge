218ccbcea96a16c4211f365870cc76917827cde0
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-10-16 16:16:31 -0400

    Show status toast after sending feedback

---
+++ src/client/dashboard/shared/js/components/Layout/Feedback/StatusToast.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Snackbar from 'material-ui/Snackbar';
+import LinearProgress from 'material-ui/LinearProgress';
+
+const style = {
+  progress: { backgroundColor: 'black' },
+  error: { color: 'rgb(228, 90, 81)' },
+};
+
+const SendingToast = () => (
+  <Snackbar
+    open
+    message={<span><LinearProgress style={ style.progress } />Sending Feedback...</span>}
+  />
+);
+
+const SentToast = () => (
+  <Snackbar open autoHideDuration={ 2000 } message="Feedback Sent!" />
+);
+
+const ErrorToast = () => (
+  <Snackbar
+    open
+    message={ <span style={ style.error }>
+      Error sending feedback. Please try again in a moment.
+    </span> }
+  />
+);
+
+const StatusToast = ({ status, idle }) => {
+  if (!idle) {
+    switch (status) {
+      case 'SENDING':
+        return <SendingToast />;
+      case 'SENT':
+        return <SentToast />;
+      case 'ERROR':
+        return <ErrorToast />;
+      default:
+        return null;
+    }
+  }
+};
+
+export default StatusToast;
