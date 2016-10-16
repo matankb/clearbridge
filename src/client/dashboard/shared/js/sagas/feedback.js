c31a8faea3f1440039eae89ef58cf3dc8275324a
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-10-16 16:16:12 -0400

    Implement reporting in feedback saga

--- src/client/dashboard/shared/js/sagas/feedback.js
+++ src/client/dashboard/shared/js/sagas/feedback.js
@@ -1,6 +1,22 @@
 import { takeEvery } from 'redux-saga';
+import { call, put, select } from 'redux-saga/effects';
+
 import { SEND_FEEDBACK, feedbackSent, feedbackSentError } from '../reducers/feedback/sending';
+import sendReport from '../report';
+
+const getData = state => state.feedback.data;
+
 function* feedback() {
+  // get feedback data from state
+  let data = yield select(getData);
+  try {
+    let response = yield call(sendReport, data.type, data);
+    yield put(feedbackSent(response.status));
+  } catch (e) {
+    yield put(feedbackSentError(e));
+  }
 }
+
 export default function* watchFeedback() {
   yield takeEvery(SEND_FEEDBACK, feedback);
+}
