8e8e72cb6ddf74f1528d0316234baa8d8b899cd2
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-10-17 12:25:54 -0400

    Clean up passport strategy

--- src/server/auth/serialize.js
+++ src/server/auth/serialize.js
@@ -1,15 +1,16 @@
 const User = require('../models/User');
 
+// serializing based on model ID, not google id.
+const serialize = (user, done) => done(null, user.id);
+
+function deserialize(id, done) {
+  User.findById(id).exec()
+    .then(user => done(null, user))
+    .catch(err => done(err));
+}
+
 module.exports = function(passport) {
-
-  // serializing based on model ID, not google id.
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(null, user);
-    });
-  });
+  // set serialization and deserialization functions
+  passport.serializeUser(serialize);
+  passport.deserializeUser(deserialize);
 };
--- src/server/auth/strategies.js
+++ src/server/auth/strategies.js
@@ -1,46 +1,27 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const User = require('../models/User');
 
+const User = require('../models/User');
 const config = require('../config');
 
-module.exports = function(passport) {
-
-  passport.use(new GoogleStrategy({
-
-    clientID: config.google.CLIENT_ID,
-    clientSecret: config.google.CLIENT_SECRET,
-    callbackURL: config.google.CALLBACK_URL,
-
-  },
-  (token, refreshToken, profile, done) => {
-    // wait for data to be passed back
-    process.nextTick(() => {
-      // lookup user based on google id
-      User.findOne({ 'google.id': profile.id }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
-        if (user) {
-          return done(null, user);
-        } else {
-          // try to find user by email instead
-          // this would mean that the user has not logged in yet
-          User.findOne({ email: profile.emails[0].value }, (err, user) => {
-            if (err) {
-              return done(err);
-            }
-            if (user) {
-              return done(null, user);
-            } else {
-              // user does not exist
-              return done('User Not Found');
-            }
-          });
-        }
-      });
-
-    });
-  }
-  ));
-
+const opts = {
+  clientID: config.google.CLIENT_ID,
+  clientSecret: config.google.CLIENT_SECRET,
+  callbackURL: config.google.CALLBACK_URL,
 };
+
+function findUserByGoogle(token, refreshToken, profile, done) {
+  const query = { email: profile.emails[0].value };
+  User.findOne(query).exec()
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done('User Not Found');
+      }
+    })
+    .catch(err => done(err));
+}
+
+const strategy = new GoogleStrategy(opts, findUserByGoogle);
+
+module.exports = passport => passport.use(strategy);
