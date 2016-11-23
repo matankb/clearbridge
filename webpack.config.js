971fd2481faedfe21b937dafa90c5e21fadacfce
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-11-23 17:30:28 -0500

    Set webpack prod env.

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin({
