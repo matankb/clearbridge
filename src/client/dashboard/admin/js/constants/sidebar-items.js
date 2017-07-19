6f7edd5160bf092e3817538924774d93ef14cbeb
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-07-19 15:55:23 +0300

    Remove unused sidebar items

--- src/client/dashboard/admin/js/constants/sidebar-items.js
+++ src/client/dashboard/admin/js/constants/sidebar-items.js
@@ -1,21 +1,13 @@
 import React from 'react';
+
 import HomeIcon from 'material-ui/svg-icons/action/home';
-import ClassIcon from 'material-ui/svg-icons/action/class';
 import SubjectIcon from 'material-ui/svg-icons/action/subject';
 import PersonIcon from 'material-ui/svg-icons/social/person';
-import SettingsIcon from 'material-ui/svg-icons/action/settings';
-import InfoIcon from 'material-ui/svg-icons/action/info';
-import HelpIcon from 'material-ui/svg-icons/action/help';
 
 const sidebarItems = [
   { name: 'Home', url: '/', icon: <HomeIcon /> },
-  { name: 'Classes', url: '/classes/', icon: <ClassIcon /> },
   { name: 'Topics', url: '/topics/', icon: <SubjectIcon /> },
   { name: 'Users', url: '/users/', icon: <PersonIcon /> },
-  { name: 'Settings', url: '/settings/', icon: <SettingsIcon /> },
-  null, // divider
-  { name: 'About', url: '/about/', icon: <InfoIcon /> },
-  { name: 'Help', url: '/help/', icon: <HelpIcon /> },
 ];
 
 export default sidebarItems;
