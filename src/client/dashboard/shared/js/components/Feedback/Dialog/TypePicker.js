cc77e18ed2c013f89600f1aa0aef6a02762797fa
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-07-20 22:37:16 +0300

    Add topic suggestion feedback type

--- src/client/dashboard/shared/js/components/Feedback/Dialog/TypePicker.js
+++ src/client/dashboard/shared/js/components/Feedback/Dialog/TypePicker.js
@@ -3,12 +3,13 @@ import React from 'react';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 
-import { SUGGESTION, PROBLEM } from '../../../report';
+import { SUGGESTION, PROBLEM, TOPIC } from '../../../report';
 
 const TypePicker = ({ onChange, value, style }) => (
   <DropDownMenu onChange={ onChange } value={ value } style={ style }>
     <MenuItem value={ SUGGESTION } primaryText="Suggestion" />
     <MenuItem value={ PROBLEM } primaryText="Problem" />
+    <MenuItem value={ TOPIC } primaryText="Topic Idea" />
   </DropDownMenu>
 );
 
--- src/server/controllers/send-report/templates.js
+++ src/server/controllers/send-report/templates.js
@@ -9,4 +9,15 @@ module.exports = {
     subject: 'Bridge Report [Bug]',
     html: `${data.comment} <br /><br /><b>Data:</b> ${JSON.stringify(navigator)}`,
   }),
+  // error report (auto-generated)
+  2: (data, navigator) => ({
+    subject: 'Bridge Report [Error]',
+    html: `There was a client-side error: ${data.error}
+           <br /><b>Browser Data:</b> ${JSON.stringify(navigator)}`,
+  }),
+  // topic suggestion
+  3: (data) => ({
+    subject: 'Bridge Report [Topic Suggestion]',
+    html: data.comment,
+  }),
 };
