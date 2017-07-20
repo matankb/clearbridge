1308896afdf3771e035a1e5028aee51687a78af3
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-07-20 12:17:01 +0300

    Display topic list on admin home page

--- src/client/dashboard/admin/js/components/Home/index.js
+++ src/client/dashboard/admin/js/components/Home/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../../../css/home.scss';
-let Home = props => {
+
+import Topics from '../Topics';
+
+const Home = () => {
   return (
     <div className="home">
+      <h1>Home</h1>
+      <Topics />
     </div>
   );
 };
+
 export default Home;
