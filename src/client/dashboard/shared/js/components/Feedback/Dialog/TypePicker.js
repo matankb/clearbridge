bae6ddadec7a07280fd6f682f1f655278bbbdc68
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-07-22 13:24:07 +0300

    Fix layout problem caused by overflow in feedback

--- src/client/dashboard/shared/js/components/Feedback/Dialog/TypePicker.js
+++ src/client/dashboard/shared/js/components/Feedback/Dialog/TypePicker.js
@@ -6,7 +6,14 @@ import MenuItem from 'material-ui/MenuItem';
 import { SUGGESTION, PROBLEM, TOPIC } from '../../../report';
 
 const TypePicker = ({ onChange, value, style }) => (
-  <DropDownMenu onChange={ onChange } value={ value } style={ style }>
+  <DropDownMenu
+    onChange={ onChange }
+    value={ value }
+    style={ style }
+    labelStyle={{
+      overflow: 'visible', // overflow: hidden breaks layout
+    }}
+  >
     <MenuItem value={ SUGGESTION } primaryText="Suggestion" />
     <MenuItem value={ PROBLEM } primaryText="Problem" />
     <MenuItem value={ TOPIC } primaryText="Topic Idea" />
