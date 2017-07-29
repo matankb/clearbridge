5258dbb1e5bb3199cfc76584a576f73077b0e8bc
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-07-29 21:27:02 +0300

    Fix formating in GridLayout

--- src/client/dashboard/student/js/components/TopicList/GridLayout.js
+++ src/client/dashboard/student/js/components/TopicList/GridLayout.js
@@ -13,86 +13,86 @@ const GridLayout = ({ items }) => {
     <Grid>
       {
         items[0] ?
-        <LargeTile>
-          { items[0] }
-        </LargeTile> :
+          <LargeTile>
+            { items[0] }
+          </LargeTile> :
         null
       }
       {
         items[1] ?
-        <LargeTile>
-          <WideTile>
-            <SmallTile>{ items[1] }</SmallTile>
-            <SmallTile>{ items[2] }</SmallTile>
-          </WideTile>
-          <WideTile>{ items[3] }</WideTile>
-        </LargeTile> :
+          <LargeTile>
+            <WideTile>
+              <SmallTile>{ items[1] }</SmallTile>
+              <SmallTile>{ items[2] }</SmallTile>
+            </WideTile>
+            <WideTile>{ items[3] }</WideTile>
+          </LargeTile> :
         null
       }
       {
         items[4] ?
-        <LargeTile>
-          <WideTile>
-            { items[4] }
-          </WideTile>
-          <WideTile>
-            <SmallTile>{ items[5] }</SmallTile>
-            <SmallTile>{ items[6] }</SmallTile>
-          </WideTile>
-        </LargeTile> :
+          <LargeTile>
+            <WideTile>
+              { items[4] }
+            </WideTile>
+            <WideTile>
+              <SmallTile>{ items[5] }</SmallTile>
+              <SmallTile>{ items[6] }</SmallTile>
+            </WideTile>
+          </LargeTile> :
         null
       }
       {
         items[7] ?
-        <LargeTile>
-          <WideTile>
-            <SmallTile>{ items[7] }</SmallTile>
-            <SmallTile>{ items[8] }</SmallTile>
-          </WideTile>
-          <WideTile>
-            <SmallTile>{ items[9] }</SmallTile>
-            <SmallTile>{ items[10] }</SmallTile>
-          </WideTile>
-        </LargeTile> :
+          <LargeTile>
+            <WideTile>
+              <SmallTile>{ items[7] }</SmallTile>
+              <SmallTile>{ items[8] }</SmallTile>
+            </WideTile>
+            <WideTile>
+              <SmallTile>{ items[9] }</SmallTile>
+              <SmallTile>{ items[10] }</SmallTile>
+            </WideTile>
+          </LargeTile> :
         null
       }
       {
         items[11] ?
-        <LargeTile>
-          <WideTile>
-            <SmallTile>{ items[11] }</SmallTile>
-            <SmallTile>{ items[12]}</SmallTile>
-          </WideTile>
-          <WideTile>
-            { items[13] }
-          </WideTile>
-        </LargeTile> :
+          <LargeTile>
+            <WideTile>
+              <SmallTile>{ items[11] }</SmallTile>
+              <SmallTile>{ items[12]}</SmallTile>
+            </WideTile>
+            <WideTile>
+              { items[13] }
+            </WideTile>
+          </LargeTile> :
         null
       }
       {
         items[14] ?
-        <LargeTile>
-          { items[14] }
-        </LargeTile> :
+          <LargeTile>
+            { items[14] }
+          </LargeTile> :
         null
       }
       {
         items[15] ?
-        <LargeTile>
-          <WideTile>{ items[15] }</WideTile>
-          <WideTile>{ items[16] }</WideTile>
-        </LargeTile> :
+          <LargeTile>
+            <WideTile>{ items[15] }</WideTile>
+            <WideTile>{ items[16] }</WideTile>
+          </LargeTile> :
         null
       }
       {
         items[16] ?
-        <LargeTile>
-          { items[16] }
-        </LargeTile> :
+          <LargeTile>
+            { items[16] }
+          </LargeTile> :
         null
       }
     </Grid>
-  )
-}
+  );
+};
 
-export default GridLayout
+export default GridLayout;
