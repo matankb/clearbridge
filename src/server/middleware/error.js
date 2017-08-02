b137681abe753e394d9a1d69168643687582f189
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-02 18:25:19 +0300

    Set status to 404 on not found page

--- src/server/middleware/error.js
+++ src/server/middleware/error.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 function handleNotFound(req, res) {
-  res.render(path.resolve(__dirname, '../../../public/errors/404.ejs'), { path: req.path });
+  res.status(404).render(path.resolve(__dirname, '../../../public/errors/404.ejs'), { path: req.path });
 }
 
 module.exports = function (app) {
