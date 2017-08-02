8f8fee8b356fd553091e048414b49bbbaa0f95a9
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-02 23:26:03 +0300

    Handle email sending errors gracefully

--- src/server/controllers/send-report/index.js
+++ src/server/controllers/send-report/index.js
@@ -12,27 +12,32 @@ const defaultOpts = {
 
 const sender = mailer.createTransport('smtps://bridge%40jcdsboston.org:Reflectbridge1@smtp.gmail.com'); // eslint-disable-line max-len
 
-function handleSent(res, error, info) {
-  let gotErrors = error || info.rejected.length > 0;
-  let errorMessage = 'Error sending message.';
+function handleSent(res, next, error, info) {
+  try {
+    let gotErrors = error || info.rejected.length > 0;
+    let errorMessage = 'Error sending message.';
 
-  if (info.rejected.length > 0) {
-    errorMessage = info.response;
+    if (info.rejected.length > 0) {
+      errorMessage = info.response;
+    }
+
+    if (gotErrors) {
+      return next(errorMessage);
+    }
+
+    res.status(200).end();
+
+  } catch (e) {
+    next(e);
   }
-
-  if (gotErrors) {
-    return res.status(500).json({ message: errorMessage });
-  }
-
-  res.status(200).end();
 }
 
 function getTemplateOptions({ type, data, navigator }) {
   return templates[type](data, navigator);
 }
 
-module.exports = function sendReport(req, res) {
+module.exports = function sendReport(req, res, next) {
   let templateOpts = getTemplateOptions(req.body);
   let opts = Object.assign({}, defaultOpts, templateOpts); // copy opts
-  sender.sendMail(opts, handleSent.bind(null, res));
+  sender.sendMail(opts, handleSent.bind(null, res, next));
 };
