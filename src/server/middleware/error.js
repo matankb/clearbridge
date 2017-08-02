bb98ab59587fc1943edde17ffcf84e3e43aae36d
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-02 23:44:04 +0300

    Create 500 error page

--- src/server/middleware/error.js
+++ src/server/middleware/error.js
@@ -4,6 +4,12 @@ function handleNotFound(req, res) {
   res.status(404).render(path.resolve(__dirname, '../../../public/errors/404.ejs'));
 }
 
+// all four arguments must be accepted for express to recognize it as internal error handler
+function internalServerError(err, req, res, next) { // eslint-disable-line no-unused-vars
+  res.status(500).render(path.resolve(__dirname, '../../../public/errors/500.ejs'));
+}
+
 module.exports = function (app) {
   app.use(handleNotFound);
+  app.use(internalServerError);
 };
