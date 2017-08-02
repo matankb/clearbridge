2aff60f612dc58365aa1917845a93c412b28f919
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-02 16:47:34 +0300

    Escape html from feedback comment

--- src/server/controllers/send-report/templates.js
+++ src/server/controllers/send-report/templates.js
@@ -1,26 +1,30 @@
 const config = require('../../config');
 
+function escapeHtml(html) {
+  return html.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;');
+}
+
 module.exports = {
   // general comment
   0: data => ({
     subject: 'Bridge Report [Comment]',
-    html: data.comment }
-  ),
+    html: escapeHtml(data.comment),
+  }),
   // bug report
   1: (data, navigator) => ({
     subject: 'Bridge Report [Bug]',
-    html: `${data.comment} <br /><br /><b>Data:</b> ${JSON.stringify(navigator)}`,
+    html: `${escapeHtml(data.comment)} <br /><br /><b>Data:</b> ${JSON.stringify(navigator)}`,
   }),
   // error report (auto-generated)
   2: (data, navigator) => ({
     subject: 'Bridge Report [Error]',
-    html: `There was a client-side error: ${data.error}
+    html: `There was a client-side error: ${escapeHtml(data.comment)}
            <br /><b>Browser Data:</b> ${JSON.stringify(navigator)}`,
   }),
   // topic suggestion
   3: (data) => ({
     subject: 'Bridge Report [Topic Suggestion]',
     to: config.feedback.suggestEmail,
-    html: data.comment,
+    html: escapeHtml(data.comment),
   }),
 };
