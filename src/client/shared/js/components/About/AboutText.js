64ba7429f8114dd812496c536aeec9d007cbe188
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-03 22:49:36 +0300

    Fix minor spacing typo in about dialog

--- src/client/shared/js/components/About/AboutText.js
+++ src/client/shared/js/components/About/AboutText.js
@@ -4,7 +4,7 @@ import '../../../css/about.less';
 const AboutText = () => (
   <div className="about">
     <h3>Created by
-      <a target="_blank" href="http://matankb.github.io/">Matan Kotler-Berkowitz</a>
+      <a target="_blank" href="http://matankb.github.io/"> Matan Kotler-Berkowitz</a>
     </h3>
     <h4>Special Thanks</h4>
     <div>
