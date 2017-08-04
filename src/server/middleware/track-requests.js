d7c4c297fc78aebcbe16555eaf4e8c7a09570fe9
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-04 17:41:21 +0300

    Correctly handle analytics for unauthenticated users

--- src/server/middleware/track-requests.js
+++ src/server/middleware/track-requests.js
@@ -2,7 +2,11 @@ const Analytic = require('../models/Analytic');
 
 module.exports = (req, res, next) => {
   const a = new Analytic();
-  a.user = req.user._id;
+  if (req.user) {
+    a.user = req.user._id;
+  } else {
+    a.user = null;
+  }
   a.path = req.originalUrl;
   a.timestamp = new Date().toUTCString();
   a.save();
