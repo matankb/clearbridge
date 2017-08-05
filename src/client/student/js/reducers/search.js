bbe6ac44f9035edecbed52bf8a2243390dd4bc8c
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-05 23:53:25 +0300

    Add open and close search actions

--- src/client/student/js/reducers/search.js
+++ src/client/student/js/reducers/search.js
@@ -3,11 +3,15 @@ export const RECIEVE_SEARCH = 'SEARCH/RECIEVE_SEARCH';
 
 export const REQUEST_SEARCH_ERROR = 'SEARCH/REQUEST_SEARCH_ERROR';
 
+export const OPEN_SEARCH = 'SEARCH/OPEN_SEARCH';
+export const CLOSE_SEARCH = 'SEARCH/CLOSE_SEARCH';
+
 const defaultState = {
   query: '',
   results: [],
   isFetching: false,
   error: null,
+  open: false,
 };
 
 export default function search(state = defaultState, action) {
@@ -18,6 +22,10 @@ export default function search(state = defaultState, action) {
       return { ...state, isFetching: false, error: null, results: action.results };
     case REQUEST_SEARCH_ERROR:
       return { ...state, isFetching: false, error: action.error };
+    case OPEN_SEARCH:
+      return { ...state, open: true };
+    case CLOSE_SEARCH:
+      return { ...state, open: false };
     default:
       return state;
   }
@@ -40,5 +48,16 @@ export function requestSearchError(error) {
   return {
     type: REQUEST_SEARCH_ERROR,
     error,
+  };
+}
+
+export function openSearch() {
+  return {
+    type: OPEN_SEARCH,
+  };
+}
+export function closeSearch() {
+  return {
+    type: CLOSE_SEARCH,
   };
 }
