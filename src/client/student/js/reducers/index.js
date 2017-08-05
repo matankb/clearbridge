b14585dbb5f7783212e48b23a7651f175ca6ef5a
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-05 23:07:25 +0300

    Create search reducers and actions

--- src/client/student/js/reducers/index.js
+++ src/client/student/js/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 import topics from './topics';
+import search from './search';
 import feedback from '../../../shared/js/reducers/feedback';
 import about from '../../../shared/js/reducers/about';
 
-export default combineReducers({ topics, feedback, about });
+export default combineReducers({ topics, search, feedback, about });
---
+++ src/client/student/js/reducers/search.js
@@ -0,0 +1,44 @@
+export const REQUEST_SEARCH = 'SEARCH/REQUEST_SEARCH';
+export const RECIEVE_SEARCH = 'SEARCH/RECIEVE_SEARCH';
+
+export const REQUEST_SEARCH_ERROR = 'SEARCH/REQUEST_SEARCH_ERROR';
+
+const defaultState = {
+  query: '',
+  results: [],
+  isFetching: false,
+  error: null,
+};
+
+export default function search(state = defaultState, action) {
+  switch (action.type) {
+    case REQUEST_SEARCH:
+      return { ...state, isFetching: true, error: null, query: action.query, results: [] };
+    case RECIEVE_SEARCH:
+      return { ...state, isFetching: false, error: null, results: action.results };
+    case REQUEST_SEARCH_ERROR:
+      return { ...state, isFetching: false, error: action.error };
+    default:
+      return state;
+  }
+}
+
+export function requestSearch(query) {
+  return {
+    type: REQUEST_SEARCH,
+    query,
+  };
+}
+export function recieveSearch(results) {
+  return {
+    type: RECIEVE_SEARCH,
+    results,
+  };
+}
+
+export function requestSearchError(error) {
+  return {
+    type: REQUEST_SEARCH_ERROR,
+    error,
+  };
+}
