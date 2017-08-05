236f885d6028a914a7ab776d02f6241fb2500432
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-05 23:41:18 +0300

    Only automatically send error reports in production

--- src/client/shared/js/onerror.js
+++ src/client/shared/js/onerror.js
@@ -1,5 +1,7 @@
 import sendReport, { ERROR } from './report';
 
-window.onerror = (msg, url, ln, col, error) => {
-  sendReport(ERROR, { error: error ? error.stack : msg });
-};
+if (process.env.NODE_ENV === 'production') {
+  window.onerror = (msg, url, ln, col, error) => {
+    sendReport(ERROR, { error: error ? error.stack : msg });
+  };
+}
