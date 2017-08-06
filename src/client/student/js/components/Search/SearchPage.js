db9eff3547cf3a9c26a00c49a77a7be4ba658d1e
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-06 11:27:00 +0300

    Make search page responsible for requesting search

--- src/client/student/js/components/Search/SearchPage.js
+++ src/client/student/js/components/Search/SearchPage.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import classnames from 'classnames';
 
-const SearchPage = props => (
-  <div className={ classnames('search-page', props.open && 'search-page-open') }>
-    This is the search page
-  </div>
-);
+import LoadableContent from '../../../../shared/js/components/LoadableContent';
+import { requestSearch } from '../../reducers/search';
+
+class SearchPage extends React.Component {
+
+  componentDidUpdate(prevProps) {
+    if (this.props.open && prevProps.query !== this.props.query) {
+      this.props.requestSearch();
+    }
+  }
+
+  render() {
+    return (
+      <div className={ classnames('search-page', this.props.open && 'search-page-open') }>
+        <LoadableContent
+          isLoading={ this.props.isFetching }
+          error={ this.props.error }
+          retry={ this.props.requestSearch }
+        >
+          { this.props.results }
+        </LoadableContent>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
+    query: state.search.query,
+    results: state.search.results,
     open: state.search.open,
+    isFetching: state.search.isFetching,
+    error: state.search.error,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    requestSearch: () => dispatch(requestSearch()),
   };
 }
 
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(SearchPage);
