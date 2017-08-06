f97460a7f48b10e75b47c136c74534839e03b944
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-06 12:51:20 +0300

    Create search results UI

--- src/client/student/js/components/Search/SearchResults/ResultCard.js
+++ src/client/student/js/components/Search/SearchResults/ResultCard.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import Paper from 'material-ui/Paper';
+
+const style = {
+  paper: {
+    display: 'flex',
+    marginTop: 30,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    height: 150,
+    maxWidth: '80%',
+    width: 680,
+  },
+};
+
 const ResultCard = ({ name, image, color, onClick }) => (
-  <div>{ name } { color }</div>
+  <div className="search-result-card">
+    <Paper style={ style.paper }>
+      <div className="image-wrap" style={{ background: color }}>
+        <img src={ image } alt={ name } />
+      </div>
+      <div className="name">
+        { name }
+      </div>
+    </Paper>
+  </div>
 );
 
 export default ResultCard;
