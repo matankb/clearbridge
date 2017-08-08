ce3aa9eb5f2943274fa1ae7d12cc8acbc4f81d5f
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-08 15:42:48 +0300

    Parse url in append trailing slash middleware

--- src/server/middleware/append-trailing-slash.js
+++ src/server/middleware/append-trailing-slash.js
@@ -2,14 +2,13 @@
 *  Appends a trailing slash to the url, then redirects
 */
 
+const url = require('url');
+
 module.exports = function(req, res, next) {
-  const [url, query] = req.originalUrl.split('?');
-  if (!url.endsWith('/')) {
-    if (query) {
-      res.redirect(`${url}/?${query}`);
-    } else {
-      res.redirect(`${url}/`);
-    }
+  const urlObj = url.parse(req.originalUrl);
+  if (!urlObj.pathname.endsWith('/')) {
+    urlObj.pathname += '/';
+    res.redirect(url.format(urlObj));
   } else {
     next();
   }
