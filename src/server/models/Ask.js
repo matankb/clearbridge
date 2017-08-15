8a5bade5e2feb4002350a7b79f7e17e3d34f1975
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-15 16:00:08 -0400

    Create Ask model

---
+++ src/server/models/Ask.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const askSchema = mongoose.Schema({
+  asker: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
+  question: String,
+  answer: String,
+  topics: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Topic' }],
+});
+
+const Ask = mongoose.model('Ask', askSchema);
+
+module.exports = Ask;
