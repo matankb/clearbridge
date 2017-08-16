3002047d72338ff0841c7ec17bb273d23255df1a
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-16 19:15:31 -0400

    Implement create ask route and controller

--- src/server/controllers/ask.js
+++ src/server/controllers/ask.js
@@ -3,3 +3,10 @@ const Ask = require('../models/Ask');
 exports.getAsks = async (req, res) => {
   res.json(await Ask.find().exec());
 };
+
+exports.createAsk = async (req, res) => {
+  const ask = new Ask();
+  ask.asker = req.user.id;
+  Object.assign(ask, req.body.data);
+  res.json(await ask.save());
+};
--- src/server/routes/api/ask.js
+++ src/server/routes/api/ask.js
@@ -6,4 +6,6 @@ const askController = require('../../controllers/ask');
 module.exports = function (router) {
 
   router.get('/asks/', ensureAuthenticated([1, 2]), wrapAsync(askController.getAsks));
+  router.post('/asks/', ensureAuthenticated(), wrapAsync(askController.createAsk));
+
 };
