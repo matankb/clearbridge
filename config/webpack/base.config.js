b09a55386a667acaef4abb2a739656a95aaee38f
Noam Raz <noamraz8@gmail.com>
2017-08-17 13:41:11 +0300

    Polyfill URL for older browsers

--- config/webpack/base.config.js
+++ config/webpack/base.config.js
@@ -6,7 +6,7 @@ const HtmlPlugin = require('html-webpack-plugin');
 const ENTRY_DIR = path.resolve(__dirname, '../../src/client/');
 
 function createEntry(name) {
-  return ['whatwg-fetch', path.resolve(ENTRY_DIR, name)];
+  return ['url-polyfill', 'whatwg-fetch', path.resolve(ENTRY_DIR, name)];
 }
 
 function createHtmlPlugin(name) {
