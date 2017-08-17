matankb/clearbridge

commit d77842f1db267114b137f4edbc16d7a5c6609581

Remove unused search reducers and actions

src/server/middleware/track-requests.js
@@ -9,6 +9,5 @@ module.exports = (req, res, next) => {
   }
   a.path = req.originalUrl;
   a.timestamp = new Date().toUTCString();
-  a.save();
   next();
 };

src/client/student/js/reducers/search.js
@@ -1,12 +1,7 @@
-export const SET_QUERY = 'SEARCH/SET_QUERY';
-
 export const REQUEST_SEARCH = 'SEARCH/REQUEST_SEARCH';
 export const RECIEVE_SEARCH = 'SEARCH/RECIEVE_SEARCH';
 
 export const REQUEST_SEARCH_ERROR = 'SEARCH/REQUEST_SEARCH_ERROR';
-
-export const OPEN_SEARCH = 'SEARCH/OPEN_SEARCH';
-export const CLOSE_SEARCH = 'SEARCH/CLOSE_SEARCH';
 
 const defaultState = {
   query: '',
@@ -18,28 +13,15 @@ const defaultState = {
 
 export default function search(state = defaultState, action) {
   switch (action.type) {
-    case SET_QUERY:
-      return { ...state, query: action.query };
     case REQUEST_SEARCH:
       return { ...state, isFetching: true, error: null, results: [] };
     case RECIEVE_SEARCH:
       return { ...state, isFetching: false, error: null, results: action.results };
     case REQUEST_SEARCH_ERROR:
       return { ...state, isFetching: false, error: action.error };
-    case OPEN_SEARCH:
-      return { ...state, open: true };
-    case CLOSE_SEARCH:
-      return { ...state, open: false };
     default:
       return state;
   }
-}
-
-export function setQuery(query) {
-  return {
-    type: SET_QUERY,
-    query,
-  };
 }
 
 export function requestSearch(query) {
@@ -59,16 +41,5 @@ export function requestSearchError(error) {
   return {
     type: REQUEST_SEARCH_ERROR,
     error,
-  };
-}
-
-export function openSearch() {
-  return {
-    type: OPEN_SEARCH,
-  };
-}
-export function closeSearch() {
-  return {
-    type: CLOSE_SEARCH,
   };
 }

src/client/student/js/sagas/search.js
@@ -1,9 +1,8 @@
-import { takeEvery, select, put } from 'redux-saga/effects';
+import { takeEvery, put } from 'redux-saga/effects';
 
 import {
   REQUEST_SEARCH,
 
-  requestSearch,
   recieveSearch,
   requestSearchError,
 } from '../reducers/search';

commit 5ceb45a39a30e4493c20cc307fa211cc9cb37796

Save tracked analytics
Due to a regression introduced in d77842f, analytics
weren't being saved. The original behavior of saving
the analytics has now been restored.

src/server/middleware/track-requests.js
@@ -2,6 +2,7 @@ const Analytic = require('../models/Analytic');
 
 module.exports = (req, res, next) => {
   const a = new Analytic();
+
   if (req.user) {
     a.user = req.user._id;
   } else {
@@ -9,5 +10,8 @@ module.exports = (req, res, next) => {
   }
   a.path = req.originalUrl;
   a.timestamp = new Date().toUTCString();
+
+  a.save();
+
   next();
 };