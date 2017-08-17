d095b0c43cbb405a7cf77b07e3f2f81c369f978d
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-17 17:32:42 -0400

    Set headers to allow body in fetch-json

--- src/client/shared/js/utils/fetch.js
+++ src/client/shared/js/utils/fetch.js
@@ -1,7 +1,14 @@
 import fetchReject from 'fetch-reject';
 
 export function fetchJson(url, opts) {
-  return fetchReject(url, { credentials: 'same-origin', ...opts })
+  return fetchReject(url, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'same-origin',
+    ...opts,
+  })
     .then(r => r.json());
 }
 
