d0bcbaad4560159d2d0cc07ebd8ac362a9121242
Matan Kotler-Berkowitz <205matan@gmail.com>
2017-08-28 17:25:01 -0400

    Transition new ask items in

--- src/client/student/js/components/TopicPage/TopicPageAsk/AskList.js
+++ src/client/student/js/components/TopicPage/TopicPageAsk/AskList.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AskList = ({ asks }) => (
-  <div className="ask-list">
+import { CSSTransitionGroup } from 'react-transition-group';
+
+const AskItem = ({ question, answer }) => (
+  <div className="ask-item">
+    <h3 className="question">{ question }</h3>
     {
-      asks.map(ask => (
-        <div className="ask-item" key={ ask._id }>
-          <h3 className="question">{ ask.question }</h3>
-          {
-            ask.answer ?
-              <div className="answer">{ ask.answer }</div> :
-              <div className="no-answer">No answer yet</div>
-          }
-        </div>
-      ))
+      answer ?
+        <div className="answer">{ answer }</div> :
+        <div className="no-answer">No answer yet</div>
     }
   </div>
 );
+
+AskItem.propTypes = {
+  question: PropTypes.string.isRequired,
+  answer: PropTypes.string,
+};
+
+AskItem.defaultProps = {
+  answer: null, // null answer means question is unanswered
+};
+
+const AskList = ({ asks }) => {
+
+  const askElements = asks.map(ask => (
+    <AskItem
+      key={ ask._id }
+      id={ ask._id }
+      question={ ask.question }
+      answer={ ask.answer }
+    />
+  ));
+
+  return (
+    <div className="ask-list">
+
+      <CSSTransitionGroup
+        transitionName="ask-item"
+        transitionEnterTimeout={ 200 }
+        transitionLeaveTimeout={ 200 }
+        component="div"
+      >
+        { askElements }
+      </CSSTransitionGroup>
+
+    </div>
+  );
+};
 
 AskList.propTypes = {
   asks: PropTypes.array.isRequired,
