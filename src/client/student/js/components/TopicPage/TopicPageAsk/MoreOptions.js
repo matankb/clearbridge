e64d5466f470af8f682ecab5aa407008738f070a
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-02-20 08:04:21 -0500

    Create help titles for more options

--- src/client/student/js/components/TopicPage/TopicPageAsk/MoreOptions.js
+++ src/client/student/js/components/TopicPage/TopicPageAsk/MoreOptions.js
@@ -6,9 +6,10 @@ import IconButton from 'material-ui/IconButton';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import ExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import HelpIcon from 'material-ui/svg-icons/action/help';
+import Divider from 'material-ui/Divider/Divider';
 
 import colors from '~/shared/js/constants/colors';
-import Divider from 'material-ui/Divider/Divider';
 
 const style = {
   button: {
@@ -26,9 +27,27 @@ const style = {
     width: 12,
     height: 12,
   },
+  // make helpIconWrap not affect positioning
+  helpIconWrap: {
+    height: 0,
+    width: 0,
+    top: 0,
+    margin: 0,
+    right: 0,
+  },
+  // apply positiong to icon
+  helpIcon: {
+    height: 24,
+    width: 24,
+    display: 'block',
+    position: 'absolute',
+    top: 0,
+    margin: 12,
+    right: 4,
+  },
 };
 
-function renderMenuItem(primaryText, onclick, checked, key) {
+function renderMenuItem(primaryText, onclick, checked, helpText, key) {
   return (
     <MenuItem
       key={ key }
@@ -41,6 +60,12 @@ function renderMenuItem(primaryText, onclick, checked, key) {
         // show invisible icon if unchecked to keep all items aligned
         !checked ? <ExpandMoreIcon color="transparent" /> : null
       }
+      rightIcon={
+        <span title={ helpText } style={ style.helpIconWrap }>
+          <HelpIcon style={ style.helpIcon } color={ colors.gray } />
+        </span>
+      }
+
     />
   );
 }
@@ -73,13 +98,24 @@ class MoreOptions extends React.Component {
 
   render() {
 
+    const helpTexts = {
+      public: 'Public questions are shown to teachers and other transfer students',
+      private: 'Private questions are only shown to your teachers',
+      nammed: 'Your teachers can see who asked "Name Shown" questions',
+      unnamed: 'Nobody can see who asked "Name Hidden" questions',
+    };
+
+    /* eslint-disable max-len */
+
     const menuItems = [
-      renderMenuItem('Public', this.props.handlePublicClick, !this.props.private, 0),
-      renderMenuItem('Private', this.props.handlePrivateClick, this.props.private, 1),
+      renderMenuItem('Public', this.props.handlePublicClick, !this.props.private, helpTexts.public, 0),
+      renderMenuItem('Private', this.props.handlePrivateClick, this.props.private, helpTexts.private, 1),
       <Divider key={ 2 } />,
-      renderMenuItem('Name Shown', this.props.handleNamedClick, this.props.named, 3),
-      renderMenuItem('Name Hidden', this.props.handleUnnamedClick, !this.props.named, 4),
+      renderMenuItem('Name Shown', this.props.handleNamedClick, this.props.named, helpTexts.nammed, 3),
+      renderMenuItem('Name Hidden', this.props.handleUnnamedClick, !this.props.named, helpTexts.unnamed, 4),
     ];
+
+    /* eslint-enable max-len */
 
     return (
       <span>
